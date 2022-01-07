c8ff54975f08a91a4f72d82515b3d800d0097332
Renan Hilario <rvahilario@gmail.com>
2022-01-06 21:25:40 -0300

    remove unused files

--- src/components/Button.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { ButtonStyled } from './styles';
-
-const Button = ({ nameButton, onClick }) => {
-	return <ButtonStyled onClick={onClick}>{nameButton}</ButtonStyled>;
-};
-
-export default Button;
