3801340e8ab3333e8dda59acd9929361e93fdf44
Renan Hilario <rvahilario@gmail.com>
2022-01-07 19:51:56 -0300

    remove unused var

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { v4 as uuidv4 } from 'uuid';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import { MainContainer } from './components/styles';
 
