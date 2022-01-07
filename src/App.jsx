a90ed015e45aad17b0f9e9f8b1e5a3a7be081702
Renan Hilario <rvahilario@gmail.com>
2022-01-07 19:07:56 -0300

    add request json from api
    this is an automatic way to populate tasks list

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { v4 as uuidv4 } from 'uuid';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
@@ -11,6 +12,17 @@ import TaskDetails from './components/TaskDetails';
 
 const App = () => {
 	const [tasks, setTasks] = useState([]);
+
+	useEffect(() => {
+		const fetchTasks = async () => {
+			const { data } = await axios.get(
+				'https://jsonplaceholder.cypress.io/todos?_limit=10'
+			);
+			setTasks(data);
+		};
+
+		fetchTasks();
+	}, []);
 
 	const handleTaskClick = (taskId) => {
 		const newTasks = tasks.map((task) => {
