c557ddf1f5c3e063db5b7c89ab45d167f6104e08
Renan Hilario <rvahilario@gmail.com>
2022-01-06 21:00:57 -0300

    add function to details button

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { RiDeleteBinLine, RiInformationLine } from 'react-icons/ri';
 
 import { ButtonTask, TaskContainer } from './styles';
 
 const Task = ({ task, handleTaskClick, handleTaskDeletion }) => {
+	const history = useHistory();
+
+	const handleDetailsClick = () => {
+		history.push(`/${task.title}`);
+	};
+
 	return (
 		<TaskContainer
 			style={task.completed ? { borderLeft: '6px solid #0a7' } : {}}
 		>
 			<div onClick={() => handleTaskClick(task.id)}>{task.title}</div>
 			<div>
-				<ButtonTask onClick={() => handleTaskDeletion(task.id)}>
+				<ButtonTask onClick={() => handleDetailsClick()}>
 					<RiInformationLine />
 				</ButtonTask>
 				<ButtonTask onClick={() => handleTaskDeletion(task.id)}>
